0532e59a9bb40dbd2b6fd801c56b1703a0edad34
dani.vilanova <dani.vilanova@abzu.ai>
2023-04-13 10:25:13 +0000

    Add run script

---
+++ run.sh
@@ -0,0 +1,24 @@
+# GPU 8 a10g 22.5GB
+torchrun --nproc_per_node=8 --master_port=20001 fastchat/train/train_lora.py \
+    --model_name_or_path bigscience/bloomz-7b1 \
+    --data_path data/ShareGPT_V3_unfiltered_cleaned_split.json \
+    --bf16 True \
+    --output_dir ./checkpoints \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1200 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True \
+    --quantize True \
+    --ddp_find_unused_parameters False
